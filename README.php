f622c7886d50dd4c6aedac5cbe525ed760f73c14
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2011-09-07 11:35:54 +0300

    Added license and read me files

---
+++ README.php
@@ -0,0 +1,67 @@
+<?php die(); ?>
+================================================================================
+COPYRIGHT AND DISCLAIMER
+================================================================================
+
+K2 Links for Advanced Link for JCE
+Copyright (C) 2009-2011  Nicholas K. Dionysopoulos / Dionysopoulos.me
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+The full text of the license can be found in the LICENSE.txt file inside all of
+our ZIP packages.
+
+================================================================================
+CONTACT AND SUPPORT
+================================================================================
+
+If you have further questions, you can contact the author directly by emailing
+at nicholas@dionysopoulos.me. Alternatively, you can send a Twitter message to
+@nikosdion. If you wish to contact me by Instant Messaging or Skype voice call,
+please leave a Personal Message to user "nicholas" on my business site
+http://www.akeebabackup.com stating the reason of communication,
+your Skype handle and timezone so that we can arrange for such communication.
+
+================================================================================
+GET INVOLVED
+================================================================================
+
+Free Software is about Freedom of Choice and collaborating with the global
+community to deliver the best software to all users. If you run into any
+problem, have written a new/improved feature you want to share with the world,
+have any suggestion/comment/wish or if you want to provide additional
+documentation or tutorials in any form (text, audio, video, etc) feel free to
+contact me by any of the available means. I take user contributions and
+requests very seriously. Everything you share back with me, even if it is a bug
+report, helps me improve this software.
+
+================================================================================
+CHANGELOG
+================================================================================
+
+Version 2.1
+--------------------------------------------------------------------------------
+! Now runs under Joomla! 1.5 and 1.7 (JCE 2.0.x required). K2 2.5 or later
+  required for operation under Joomla! 1.7 or later.
+# Would not run under Joomla! 1.7 and K2 2.5
+# Users had access to all content items, irresspective of their access level
+
+Version 2.0
+--------------------------------------------------------------------------------
+! This version now only supports JCE 2.0.x
++ Support for K2 tags
+
+Version 1.0
+--------------------------------------------------------------------------------
+! Initial release for Joomla! 1.5 (JCE 1.5.x) only
